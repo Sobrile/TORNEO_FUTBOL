c727bb2c33261a830d66772af96a982118d8ee35
Juan Andres Sobrile <juan_sobrile@hotmail.com>
2018-10-27 23:39:35 -0300

    .

---
+++ Torneos_Futbol/Base de Datos/inserts_prueba.sql
@@ -0,0 +1,28 @@
+------------------------ TORNEOS
+INSERT INTO dbo.Torneo VALUES ('Torneo 1', 1);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 2', 1);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 3', 0);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 4', 1);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 5', 0);
+------------------------ EQUIPOS
+INSERT INTO Dbo.Equipo VALUES ( 'Racing', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'River', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'Boca', 100, 2);
+INSERT INTO Dbo.Equipo VALUES ( 'Velez', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'San Lorenzo', 100, 3);
+INSERT INTO Dbo.Equipo VALUES ( 'Rosario Central', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'Barcelona', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Real Madrid', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Villareal', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Valencia', 100, 4);
+------------------------ JUGADORES
+insert into dbo.Jugador values ( 'Sebastian',  'Saja', 35, 1);
+insert into dbo.Jugador values ( 'Gabriel',  'Mercado', 28, 2);
+insert into dbo.Jugador values ( 'Fernando',  'Gago', 29, 3);
+insert into dbo.Jugador values ( 'Sebastian',  'Dominguez', 28, 4);
+insert into dbo.Jugador values ( 'Nestor',  'Ortigoza', 30, 5);
+insert into dbo.Jugador values ( 'Marco',  'Ruben', 28, 6);
+insert into dbo.Jugador values ( 'Karim',  'Benzema', 27, 8);
+insert into dbo.Jugador values ( 'Luciano',  'Vietto', 21, 9);
+insert into dbo.Jugador values ( 'Rodrigo',  'De Paul', 21, 10);
+insert into dbo.Jugador values ( 'Lionel',  'Messi', 28, 7);
